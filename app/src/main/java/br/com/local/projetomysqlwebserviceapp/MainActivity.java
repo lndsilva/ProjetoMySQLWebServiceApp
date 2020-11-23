84272d07663faacb72bbfabd3cf204cfbe59f42d
Laercio-Note <lndsilva@hotmail.com>
2020-11-23 11:53:05 -0300

    CRUD - insert e select

--- app/src/main/java/br/com/local/projetomysqlwebserviceapp/MainActivity.java
+++ app/src/main/java/br/com/local/projetomysqlwebserviceapp/MainActivity.java
@@ -3,12 +3,129 @@ package br.com.local.projetomysqlwebserviceapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtCodProd, txtNomeProd, txtPrecoProd, txtFabricante;
+    Button btnBuscar, btnCadastrar, btnAlterar, btnExcluir;
+
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtCodProd = findViewById(R.id.txtCodProd);
+        txtNomeProd = findViewById(R.id.txtNomeProd);
+        txtPrecoProd = findViewById(R.id.txtPrecoProd);
+        txtFabricante = findViewById(R.id.txtFabricante);
+
+        btnBuscar = findViewById(R.id.btnBuscar);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+        btnAlterar = findViewById(R.id.btnAlterar);
+        btnExcluir = findViewById(R.id.btnExcluir);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cadastrarProduto("http://192.168.100.13/webservice/cadastrar_produtos.php");
+            }
+        });
+
+        btnBuscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buscarProdutos("http://192.168.100.16/webservice/buscar_produtos.php?codProd=" + txtCodProd.getText().toString() + "");
+            }
+        });
+    }
+
+    public void cadastrarProduto(String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(),
+                        "Cadastrado com sucesso!!!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),
+                        error.toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("codProd", txtCodProd.getText().toString());
+                parametros.put("nomeProd", txtNomeProd.getText().toString());
+                parametros.put("precoProd", txtPrecoProd.getText().toString());
+                parametros.put("fabricanteProd", txtFabricante.getText().toString());
+
+                return parametros;
+            }
+        };
+
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    public void buscarProdutos(String URL) {
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(URL, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                Toast.makeText(getApplicationContext(),
+                        "passei",
+                        Toast.LENGTH_SHORT).show();
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        txtNomeProd.setText(jsonObject.getString("codProd"));
+                        txtPrecoProd.setText(jsonObject.getString("nomeProd"));
+                        txtFabricante.setText(jsonObject.getString("fabricanteProd"));
+
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(),
+                                e.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),
+                        "Erro ao conectar ao banco de dados",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
     }
 }
